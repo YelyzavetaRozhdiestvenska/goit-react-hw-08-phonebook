107e24cf7f7869ea549b49862e728ac10d100b94
YelyzavetaRozhdiestvenska <123411730+YelyzavetaRozhdiestvenska@users.noreply.github.com>
2023-12-21 22:30:13 +0000

    Deploying to gh-pages from @ YelyzavetaRozhdiestvenska/goit-react-hw-08-phonebook@3e3937a9c4bdba811c92f23d5149167a76537813 🚀

---
+++ static/js/266.817052b7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var i=n(184),r={container:{minHeight:"calc(100vh - 50px)",display:"flex",justifyContent:"center"},title:{fontWeight:500,fontSize:48,marginRight:"24px",marginLeft:"auto",color:"#2a363b"}};function o(){return(0,i.jsx)("div",{style:r.container,children:(0,i.jsx)("h1",{style:r.title,children:"My Phonebook "})})}}}]);
+//# sourceMappingURL=266.817052b7.chunk.js.map
